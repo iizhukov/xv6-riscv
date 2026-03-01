fe17b811e2088fbf9c626cd80662033406854657
iizhukov <iizhukov@icloud.com>
2026-03-01 15:13:07 +0300

    task 1

--- Makefile
+++ Makefile
@@ -145,6 +145,8 @@ UPROGS=\
 	$U/_logstress\
 	$U/_forphan\
 	$U/_dorphan\
+	$U/_task1a\
+	$U/_task1b\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/task1b.c
@@ -0,0 +1,31 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char **argv)
+{
+    int pid = fork();
+    int status;
+
+    if (pid == 0)
+    {
+        pause(5 * 10);
+        return 1;
+    }
+
+    if (pid < 0)
+    {
+        printf("fork failed\n");
+        return 1;
+    }
+    
+    printf("parent pid: %d\n", getpid());
+    printf("child pid: %d\n", pid);
+
+    if (kill(pid) == -1)
+        return 1;
+
+    pid = wait(&status);
+    printf("child %d is done with %d status\n", pid, status);
+    return 0;
+}
